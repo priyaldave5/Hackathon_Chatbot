018d8d62c0c0a1de80141de1ba0a24e6266c5a8a
priyaldave5 <priyaldave007@gmail.com>
2025-09-16 20:27:56 +0530

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,70 @@
+import os
+import json
+import datetime
+import csv
+import nltk
+import ssl
+import streamlit as st
+import random
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+
+ssl._create_default_https_context = ssl._create_unverified_context
+nltk.data.path.append(os.path.abspath("nltk_data"))
+nltk.download('punkt')
+
+file_path = os.path.abspath("./intents.json")
+with open(file_path, "r") as file:
+    intents = json.load(file)
+
+vectorizer = TfidfVectorizer(ngram_range=(1, 4))
+clf = LogisticRegression(random_state=0, max_iter=10000)
+
+tags = []
+patterns = []
+for intent in intents:
+    for pattern in intent['patterns']:
+        tags.append(intent['tag'])
+        patterns.append(pattern)
+
+x = vectorizer.fit_transform(patterns)
+y = tags
+clf.fit(x, y)
+
+def chatbot(input_text):
+    input_text = vectorizer.transform([input_text])
+    tag = clf.predict(input_text)[0]
+    for intent in intents:
+        if intent['tag'] == tag:
+            response = random.choice(intent['responses'])
+            return response
+        
+counter = 0
+
+def main():
+    global counter
+    st.title("WELCOME! This is a chatbot created by Priyal")
+
+    menu = ["Home","About"]
+    choice = st.sidebar.selectbox("Menu", menu)
+
+    if choice == "Home":
+        
+        st.write(" Please type a message and press Enter to start the conversation.")
+    
+        counter += 1
+        user_input = st.text_input("You:", key=f"user_input_{counter}")
+
+        if user_input:
+
+            user_input_str = str(user_input)
+
+            response = chatbot(user_input)
+            st.text_area("Chatbot:", value=response, height=120, max_chars=None, key=f"chatbot_response_{counter}")
+            
+
+    elif choice == "About":
+        st.write("PROBLEM STATEMENT :To built a conversational chatbot that can interact with users, understand their queries and give appropriate response to them. The chatbot should be able to understand NLP, process it and deliver coherent replies using NLP techniques.A chatbot is a computer program that simulates and processes human conversation, allowing humans to interact with digital devices as if they were communicating with a real person.")
+   
+if __name__ == '__main__':
+    main()
